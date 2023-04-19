3fedcf4b83bbba39fa2c54a397082c9e19e41f5c
maqamylee0 <maqamylee0@gmail.com>
2023-04-19 08:34:20 -0700

    starter files

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main- starting function
+ * @ac: number of arguments
+ * @argv: array of pointers to strings
+ *
+ * Return: 0 for success or non zero for failure
+ *
+ */
+
+
+int main(int ac, char **argv)
+{
+	(void)(ac);
+	(void)(argv);
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef __SHELL_H_
+#define __SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
