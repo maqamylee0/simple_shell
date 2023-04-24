9ebadbe3fe9bc7f65767a530c523db181ebc5aae
maqamylee0 <maqamylee0@gmail.com>
2023-04-24 06:35:56 -0700

    fix buffer_loc , EOF and check NULL before buffer alloc

--- _getline.c
+++ _getline.c
@@ -16,18 +16,19 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 	size_t buffer_size = *n;
 	size_t read_bytes = 0;
 	int fd = fileno(stream);
-
-	buffer = malloc(buffer_size);
+	char *buffer_loc = NULL;
 
 	while (lineptr == NULL || n == NULL || stream == NULL)
 	{
 		return (-1);
 	}
+	buffer = allocate(buffer_size);
+	buffer_loc = buffer;
 	if (buffer_size == 0)
 	{
-		buffer_size = 10;
+		buffer_size = 100;
 	}
-	while ((bytes_read = read(fd, buffer, buffer_size - read_bytes)) > 0)
+	while ((bytes_read = read(fd, buffer_loc, buffer_size - read_bytes)) > 0)
 	{
 		read_bytes += bytes_read;
 		if (read_bytes >= buffer_size)
@@ -39,9 +40,9 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 		}
 		if (buffer[read_bytes - 1] == '\n')
 			break;
-		buffer += read_bytes;
+		buffer_loc += read_bytes;
 	}
-	if (bytes_read == -1)
+	if (bytes_read == -1 || bytes_read == 0)
 	{
 		free(buffer);
 		return (-1);
